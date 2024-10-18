6d4c22eac5ab3fa6814fea7354c28fdca5bac615
BuildTools <unconfigured@null.spigotmc.org>
2024-10-18 02:53:16 -0400

    Update 1.0.3

--- src/main/java/net/lettuce/mario64paintings/core/Mario64Paintings.java
+++ src/main/java/net/lettuce/mario64paintings/core/Mario64Paintings.java
@@ -1,12 +1,7 @@
-package net.lettuce.templatemod.core;
+package net.lettuce.mario64paintings.core;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,35 +9,19 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
-@Mod(TemplateMod.MOD_ID)
-public class TemplateMod {
-    public static final String MOD_ID = "template_mod";
-    public static final Logger LOGGER = LogUtils.getLogger();
+@Mod(Mario64Paintings.MOD_ID)
+public class Mario64Paintings {
+    public static final String MOD_ID = "mario_64_paintings";
 
-    public TemplateMod() {
+    public Mario64Paintings() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
-        TemplateItems.register(modEventBus);
-        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-    }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(TemplateItems.TEMPLATE_ITEM);
-
-            event.getEntries().putAfter(
-                    Items.LEATHER_HORSE_ARMOR.getDefaultInstance(),
-                    TemplateItems.TEMPLATE_ITEM.get().getDefaultInstance(),
-                    CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS
-            );
-        }
     }
 
     @SubscribeEvent
